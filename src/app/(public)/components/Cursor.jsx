a9a024d75abfbf34ab5d744257ad4825433fc3fd
rohan26ir <rohan26ir@gmail.com>
2025-08-28 06:39:09 +0600

    added cursor

--- src/app/(public)/components/Cursor.jsx
+++ src/app/(public)/components/Cursor.jsx
@@ -1,16 +1,35 @@
-'use client'
-import React from 'react';
+"use client";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import React from "react";
+import logoIBM from "../../../../public/logo/ibm.png";
+import logomicrosoft from "../../../../public/logo/microsoft.png";
+
+// Dynamically import Marquee
+const Marquee = dynamic(() => import("react-fast-marquee"), { ssr: false });
 
 const Cursor = () => {
-    return (
-        <div>
-            <div>
-                <div>
-                    <h2>Cursor</h2>
-                </div>
+  const imageLogo = [
+    { id: 1, src: logoIBM, alt: "IBM Logo" },
+    { id: 2, src: logomicrosoft, alt: "Microsoft Logo" },
+  ];
+
+  // Repeat logos for a longer marquee track
+  const repeatedLogos = Array(3).fill(imageLogo).flat();
+
+  return (
+    <div className="h-28 flex items-center w-full">
+      <Marquee pauseOnHover={true} autoFill={true} speed={50}>
+        <div className="flex items-center gap-5 md:gap-10 lg:gap-32">
+          {repeatedLogos.map((logo, index) => (
+            <div key={index}>
+              <Image src={logo.src} alt={logo.alt} width={150} height={150} />
             </div>
+          ))}
         </div>
-    );
+      </Marquee>
+    </div>
+  );
 };
 
 export default Cursor;
